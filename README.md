275e6f9d257063697e87262a31ce415999513223
Prakhar Verma <connect@iprakharv.com>
2024-05-27 23:01:56 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# lexed_ai_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/lexed_ai_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lexed_ai_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
